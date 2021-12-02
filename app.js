c66eb80aeec470b246606d0580b667be2cac51fc
Ronya <klmirg6@gmail.com>
2021-12-01 18:04:38 -0600

    updated the html-template

--- app.js
+++ app.js
@@ -1,8 +1,6 @@
+const fs = require('fs');
 const inquirer = require('inquirer');
-// const fs = require('fs');
-// const generatePage = require('./src/page-template.js');
-
-// const pageHTML = generatePage(name, github);
+const generatePage = require('./src/page-template.js');
 
 const promptUser = () => {
   return inquirer.prompt([
@@ -132,17 +130,15 @@ const promptProject = portfolioData => {
       return portfolioData;
     }
   });
- 
 };
 
 promptUser()
 .then(promptProject)
 .then(portfolioData => {
-  console.log(portfolioData);
+  const pageHTML = generatePage(portfolioData);
+  fs.writeFile('./index.html', pageHTML, err => {
+    if (err) throw new Error (err);
+  
+    console.log('Portfolio complete! Check out index.html to see the output!')
+  });
 });
-
-// fs.writeFile('./index.html', pageHTML, err => {
-//   if (err) throw err;
-
-//   console.log('Portfolio complete! Check out index.html to see the output!')
-// });
